1f444929c43688722faa5702f86844f8c81371d5
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-03-18 17:40:50 +0100

    end hackXD

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("YO")
+/*
+1.richiedere Nome
+2.Richiedere Cognome
+3.Richiedere colore preferito
+4.stampare nomeCognomeColore21
+*/
+
+var firstName = prompt("What's your name?")
+//console.log (firstName);
+var surname = prompt("What's your Surname?");
+//console.log (surname);
+var color = prompt("what's your favorite color?");
+var pw = firstName + " " + surname + " " + color + 21;
+document.getElementById("content").innerHTML = pw + " u got HACKED!!!";
+
